0a1e8ca39d3af1bdce68fbc92c225ffc6a00efe9
JahazielGuzman <jahaziel.guzman93@gmail.com>
2019-06-10 17:55:05 -0400

    solved prime?

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def prime?(n)
+	
+	if n == 2
+		isprime = true
+	elsif n % 2 == 0 || n <= 1
+		isprime = false
+	else
+		i = 3
+		isprime = true
+		while i < n && isprime == true
+			if n % i == 0
+				isprime = false
+			end 
+			i += 1
+		end
+
+	end
+
+	isprime
+end
