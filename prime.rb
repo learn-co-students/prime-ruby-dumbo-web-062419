b0f5255df5a2046ebbef3fbb67a5946cb81593c8
Aliciea D Suechin <adr330@nyu.edu>
2019-06-14 16:57:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,4 +1,9 @@
 # Add  code here!
-def prime(2)
-  return true 
-end
+def prime?(num)
+  return false if num < 2 
+  range = 2..num-1 
+  range.each do |x|
+    return false if num % x==0 
+  end 
+  true 
+end 
