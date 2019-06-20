4b56f7b04805b7b4a55f26ea5b78d7e5182100eb
John Fajardo <fajardocj@gmail.com>
2019-06-20 16:15:52 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(int)
+    return false if int < 2
+    return true if int == 2 || int == 3
+    checks = (2..(int-1))
+
+    checks.each do |check|
+        return false if int % check == 0
+    end
+    
+    true    
+end
